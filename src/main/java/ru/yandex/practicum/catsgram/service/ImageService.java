d119ca471fea371249ba337a43aa8b53ba8da287
Максим Торжков <trzm@h.hermes-industries.local>
2025-06-30 14:54:18 +0300

    feat: add ImageService and Controller

---
+++ src/main/java/ru/yandex/practicum/catsgram/service/ImageService.java
@@ -0,0 +1,85 @@
+package ru.yandex.practicum.catsgram.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
+import ru.yandex.practicum.catsgram.model.Image;
+import ru.yandex.practicum.catsgram.model.Post;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Instant;
+import java.util.ConcurrentModificationException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ImageService {
+    private final PostService postService;
+
+    private final Map<Long, Image> images = new HashMap<>();
+    private final String imageDirectory = "C:\\Users\\trzm\\Pictures\\projpic";
+
+    public List<Image> getPostImages(Long postId) {
+        return images.values()
+                .stream()
+                .filter(image -> image.getPostId().equals(postId))
+                .collect(Collectors.toList());
+    }
+
+    public List<Image> saveImages(Long postId, List<MultipartFile> files) {
+        return files.stream().map(file -> saveImage(postId, file)).collect(Collectors.toList());
+    }
+
+    private Image saveImage(Long postId, MultipartFile file) {
+        Post post = postService.findPostById(postId)
+                .orElseThrow(() -> new ConditionsNotMetException("Указанный пост не найден"));
+
+        Path filePath = saveFile(file, post);
+        Long imageId = getNextId();
+
+        Image image = new Image();
+        image.setId(imageId);
+        image.setFilePath(filePath.toString());
+        image.setPostId(postId);
+        image.setOriginalFileName(file.getOriginalFilename());
+
+        images.put(imageId, image);
+
+        return image;
+    }
+
+    private Path saveFile(MultipartFile file, Post post) {
+        try {
+            String uniqueFileName = String.format("%d.%s", Instant.now().toEpochMilli(),
+                    StringUtils.getFilenameExtension(file.getOriginalFilename()));
+
+            Path uploadPath = Paths.get(imageDirectory, String.valueOf(post.getAuthorId()), post.getId().toString());
+            Path filePath = uploadPath.resolve(uniqueFileName);
+
+            if (!Files.exists(uploadPath)) {
+                Files.createDirectories(uploadPath);
+            }
+            file.transferTo(filePath);
+            return filePath;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private long getNextId() {
+        long currentMaxId = images.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}
