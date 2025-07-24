51e96d2b4484451331398b468eff96851ebacaa3
Nia <eugeniamashina@gmail.com>
2025-07-24 15:55:33 +0300

    feat: add db func

--- src/main/java/ru/yandex/practicum/catsgram/service/UserService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/UserService.java
@@ -1,9 +1,12 @@
 package ru.yandex.practicum.catsgram.service;
 
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.catsgram.dal.UserRepository;
+import ru.yandex.practicum.catsgram.dto.UserDto;
 import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
 import ru.yandex.practicum.catsgram.exception.DuplicatedDataException;
 import ru.yandex.practicum.catsgram.exception.NotFoundException;
+import ru.yandex.practicum.catsgram.mapper.UserMapper;
 import ru.yandex.practicum.catsgram.model.User;
 
 import java.time.Instant;
@@ -12,6 +15,18 @@ import java.util.stream.Collectors;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<UserDto> getUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(UserMapper::mapToUserDto)
+                .collect(Collectors.toList());
+    }
 
     private final Map<Long, User> users = new HashMap<>();
 
